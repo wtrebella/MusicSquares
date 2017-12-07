4077e48ec028777fa23df61fbe6c62294050d4fe
Whitaker Trebella <wtrebella@gmail.com>
2017-12-07 17:39:14 -0600

    - added a butt plug thing for the ring to go on

---
+++ MusicSquares_Unity/Assets/Scripts/Ring.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour {
+
+	void Awake()
+	{
+		GetComponent<Rigidbody> ().isKinematic = true;
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody> ().isKinematic = false;
+		}
+	}
+}
